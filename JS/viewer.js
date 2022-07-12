6c5eca11f8f3add8f66069715e6f41a739304881
Low Poly Panda <llsunyer@gmail.com>
2022-07-12 18:07:13 +0200

    Add basic three code

--- JS/viewer.js
+++ JS/viewer.js
@@ -9,20 +9,58 @@ function open() {
   document.getElementById("sidebar").style.width = "15rem";
   document.getElementById("menuClose").style.display = "";
   document.getElementById("menuOpen").style.display = "none";
-};
+}
 function close() {
   console.log("tancar" + isOnDiv);
   document.getElementById("sidebar").style.width = "2.5rem";
   document.getElementById("menuClose").style.display = "none";
   document.getElementById("menuOpen").style.display = "";
-};
+}
 //IMPORT PROJECTS INFO
 import { projectArray } from "./projects.js";
 const urlParam = new URLSearchParams(window.location.search);
-const id = urlParam.get('id');
+const id = urlParam.get("id");
 const title = "/ " + projectArray[id].title;
 document.getElementById("title").innerText = title;
 //THREE
-
-
-
+//Three import
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  MeshPhongMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+//Scene
+const scene = new Scene();
+//Objects
+const xSide = parseInt(id) + 1;
+const geometry = new BoxGeometry(xSide, 1, 1);
+const material = new MeshBasicMaterial({ color: "white" });
+const box = new Mesh(geometry, material);
+scene.add(box);
+//Camera
+////////
+const sizes = {
+  width: 800,
+  height: 600,
+};
+////////////
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+//Renderer
+const canvas = document.getElementById("viewer-container");
+const renderer = new WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+//Animate
+function animate() {
+  box.rotation.x += 0.01;
+  box.rotation.z += 0.01;
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+}
+animate();
